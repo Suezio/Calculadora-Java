c1ff9ac6c39f592745a7b29e7eed9d06ae2e9125
suezio <sueziooliveira@gmail.com>
2021-11-30 21:33:14 -0300

    subindo metodo soma

---
+++ src/main/java/clases/Operacao.java
@@ -0,0 +1,10 @@
+package clases;
+
+public class Operacao {
+
+    public  double soma(double num1, double num2){
+        double resultado;
+        resultado = num1+num2;
+        return resultado;
+    }
+}
---
+++ src/main/java/clases/Principal.java
@@ -0,0 +1,22 @@
+package clases;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        double num1,num2,resultado;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        num1 = entrada.nextDouble();
+
+        System.out.println("Digite OUTRO número: ");
+        num2 = entrada.nextDouble();
+
+
+
+    }
+
+}
